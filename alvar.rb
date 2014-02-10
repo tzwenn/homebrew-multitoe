667cde6f7baeb956af10e3e72a291e7d6a265888
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2014-02-10 12:17:33 +0100

    Use brew's c++11 flag, full name of opencv formula

--- alvar.rb
+++ alvar.rb
@@ -5,11 +5,15 @@ class Alvar < Formula
   url 'https://github.com/tzwenn/alvar.git', :branch => 'master'
   version '2.0.0'
 
-  depends_on 'opencv'
+  depends_on 'homebrew/science/opencv'
   depends_on 'tinyxml'
   depends_on 'cmake' => :build
 
+  option :cxx11
+
   def install
+    ENV.cxx11 if build.cxx11?
+
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
